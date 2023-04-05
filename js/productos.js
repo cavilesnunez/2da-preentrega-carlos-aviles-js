f81959765fdfba4fa19a3ecda1b2bd0cf71f3a4a
Carlos Aviles Nuñez <carlosfaviesn@gmail.com>
2023-04-04 18:34:41 -0600

    Primer Commit
    Entrega 2 Carlos Aviles Javascript

---
+++ js/productos.js
@@ -0,0 +1,51 @@
+const productos = [
+    {
+        id: 1,
+        foto: "https://kbd.news/pic/2022/107/1805.jpg",
+        nombre: "42Azalia",
+        precio: 1500
+    },
+    {
+        id: 2,
+        foto: "https://kbd.news/pic/2022/71/1334.jpg",
+        nombre: "Banime40",
+        precio: 1850
+    },
+    {
+        id: 3,
+        foto: "https://kbd.news/pic/2022/92/1604.jpg",
+        nombre: "Beyblock20",
+        precio: 1350
+    },
+    {
+        id: 4,
+        foto: "https://kbd.news/pic/2022/107/1804.jpg",
+        nombre: "Ffkb with led ring",
+        precio: 2100
+    },
+    {
+        id: 5,
+        foto: "https://kbd.news/pic/2022/81/1467.jpg",
+        nombre: "Freaku4x",
+        precio: 1950
+    },
+    {
+        id: 6,
+        foto: "https://kbd.news/pic/2022/91/1592.jpg",
+        nombre: "Keezy boost40",
+        precio: 2300
+    },
+    {
+        id: 7,
+        foto: "https://kbd.news/pic/2022/103/1740.jpg",
+        nombre: "Kunai",
+        precio: 1740
+    }
+    ,
+    {
+        id: 8,
+        foto: "https://kbd.news/pic/2023/119/1968.jpg",
+        nombre: "Dilemma v2",
+        precio: 3550
+    }
+];
